beb842823ec68f14d853b82b2cdfda977210f6b7
harold montes <haroldjosehd24@gmail.com>
2025-04-13 15:54:30 -0400

    Fix favoritebutton component to update global state with latest changes when toggling favorite status

--- src/components/FavoriteButton/FavoriteButton.tsx
+++ src/components/FavoriteButton/FavoriteButton.tsx
@@ -1,36 +1,55 @@
 import { useRef } from "react";
 import { useLibrary } from "../../context/LibraryContext";
 import { BookPropType } from "../../types/books/book.type";
-import { ImStarFull  } from "react-icons/im";
+import { ImStarFull } from "react-icons/im";
 
 type FavoriteButtonProps = {
-    book: BookPropType;
-    index: number;
-}
+  book: BookPropType;
+  index: number;
+};
 
-const FavoriteButton = ({book, index}: FavoriteButtonProps) => {
+const FavoriteButton = ({ book, index }: FavoriteButtonProps) => {
+  const { isFavoriteBook, getOneBook, setBooks } = useLibrary();
 
-    const { isFavoriteBook } = useLibrary();
-    
-    const myRef = useRef<(HTMLButtonElement | null)[]>([]);
+  const myRef = useRef<(HTMLButtonElement | null)[]>([]);
 
-
-
-    const handleFavorite = async (index: number): Promise<void> => {
-        const button = myRef.current[index]; 
-    
-        if (button) {
-          if (button.classList.contains('btn-favorite-active')) {
-            button.classList.remove('btn-favorite-active');
-            button.classList.add('btn-favorite');
-          } else {
-            button.classList.add('btn-favorite-active');
-            button.classList.remove('btn-favorite');
-          }
-          await isFavoriteBook(book);
-        }
+  const handleFavoriteState = (book: BookPropType) => {
+    setBooks((prevBooks) => {
+      if (prevBooks === null) {
+        return null; 
       }
-    
+      const updatedBooks = prevBooks.data.map((prevBook) => {
+        return prevBook.id === book.id
+          ? { ...prevBook, isFavorite: book.isFavorite }
+          : prevBook;
+      });
+      return { ...prevBooks, data: updatedBooks };
+    });
+  };
+
+  const handleFavorite = async (index: number): Promise<void> => {
+    const button = myRef.current[index];
+
+    if (button) {
+      if (button.classList.contains("btn-favorite-active")) {
+        button.classList.remove("btn-favorite-active");
+        button.classList.add("btn-favorite");
+      } else {
+        button.classList.add("btn-favorite-active");
+        button.classList.remove("btn-favorite");
+      }
+      await isFavoriteBook(book);
+
+      //se obtiene del contexto el libro con el dato actualizado
+      const updatedBookResponse = await getOneBook(Number(book.id));
+
+      //se agrega al estado para react renderice la pagina con el nuevo cambio
+      handleFavoriteState({
+        ...book,
+        isFavorite: updatedBookResponse.isFavorite,
+      });
+    }
+  };
 
   return (
     <button
