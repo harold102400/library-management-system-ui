370a106c782c2cf9439d61078d386233d6a02d47
hmontes <haroldjosehd24@gmail.com>
2025-04-17 19:41:37 -0400

    Use shared config.ts for env constants instead of redefining in component

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -7,10 +7,7 @@ import {
 } from "react";
 import axios from "axios";
 import { registerUser } from "../services/apiFunctions";
-
-const TOKEN_KEY = import.meta.env.VITE_TOKEN_KEY;
-const API_URL = import.meta.env.VITE_API_URL
-
+import { API_URL, TOKEN_KEY } from "../config/config";
 
 export type UserContextValue = {
     onLogin: (input: string, password: string) => Promise<void>;
