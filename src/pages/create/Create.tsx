7c638e565579cc37fa549c3f37aef0e884303f7a
hmontes <haroldjosehd24@gmail.com>
2025-04-18 17:46:46 -0400

    Redesign Create and Edit forms as modals for improved layout, and update .gitignore to keep local files without uploading them to GitHub by saving them as .txt

--- src/pages/create/Create.tsx
+++ src/pages/create/Create.tsx
@@ -1,13 +1,11 @@
 import { useForm } from "react-hook-form";
-import { FieldSet } from "../../components/FormFields/FieldSet";
-import { FormField } from "../../components/FormFields/FormField";
 import { createBook } from "../../services/apiFunctions";
 import { BookPropType } from "../../types/books/book.type";
 import { useAuth } from "../../context/UserContext";
 import { handlError } from "../../components/ErrorAlert/ErrorAlert";
 import { useNavigate, Link } from "react-router-dom";
-import "../../components/FormFields/FormStyles.css";
 import { handleApiError } from "../../utils/handleApiErrors";
+import "../../components/FormFields/FormStyles.css";
 
 export const Create = () => {
   const {
@@ -20,13 +18,12 @@ export const Create = () => {
       author: "",
       year: "",
       isFavorite: 0,
-      genre: '[]',
+      genre: "[]",
     },
   });
   const { authState: user_id } = useAuth();
   const navigate = useNavigate();
   const id = user_id.user_id;
-  
 
   const submitForm = async (data: BookPropType) => {
     try {
@@ -38,128 +35,124 @@ export const Create = () => {
       await createBook(newData);
       navigate("/books");
     } catch (error: unknown) {
-      const errorMessage = handleApiError(error)
-      handlError(errorMessage)
+      const errorMessage = handleApiError(error);
+      handlError(errorMessage);
     }
   };
-  
-  
+
   return (
-    <div className="form-container">
-      <h1 className="form-header">Create form</h1>
-      <form onSubmit={handleSubmit(submitForm)} className="form">
-        <FieldSet>
-          <FormField label="Title" error={errors.title?.message}>
-            <input
-              type="text"
-              placeholder="Title"
-              className="form-input"
-              {...register("title", {
-                required: "This field is required and cannot be empty",
-                setValueAs: (value) => value.trim(),
-                pattern: {
-                  value: /^[A-Za-zÀ-ÿ0-9\s.@]+$/,
-                  message: "Title must contain only letters and spaces",
-                },
-                maxLength: {
-                  value: 200,
-                  message: "Title cannot be longer than 200 characters",
-                },
-              })}
-            />
-          </FormField>
-          <FormField label="Author" error={errors.author?.message}>
-            <input
-              type="text"
-              placeholder="Author"
-              className="form-input"
-              {...register("author", {
-                required: "This field is required and cannot be empty",
-                setValueAs: (value) => value.trim(),
-                pattern: {
-                  value: /^[A-Za-zÀ-ÿ0-9\s.@]+$/,
-                  message: "Title must contain only letters and spaces",
-                },
-                maxLength: {
-                  value: 100,
-                  message: "Title cannot be longer than 100 characters",
-                },
-              })}
-            />
-          </FormField>
-
-          <FormField label="Year" error={errors.year?.message}>
-            <input
-              type="date"
-              placeholder="Year"
-              className="form-input"
-              {...register("year", {
-                required: "This field is required and cannot be empty",
-              })}
-            />
-          </FormField>
-
-          <div className="categories">
-            <p className="categories-header">Categories</p>
-            {["Fantasy", "Adventure", "Science Fiction", "Mystery"].map(
-              (genre, index) => {
-                const checkboxId = `cbx-genre-${index}`;
-                return (
-                  <div className="checkbox-wrapper-15" key={genre}>
-                    <input
-                      className="inp-cbx"
-                      id={checkboxId}
-                      type="checkbox"
-                      style={{ display: "none" }}
-                      value={genre}
-                      {...register("genre")} 
-                    />
-                    <label className="cbx" htmlFor={checkboxId}>
-                      <span>
-                        <svg width="12px" height="9px" viewBox="0 0 12 9">
-                          <polyline points="1 5 4 8 11 1"></polyline>
-                        </svg>
-                      </span>
-                      <span>{genre}</span>
-                    </label>
-                  </div>
-                );
-              }
-            )}
-          </div>
-
-          {/* <FormField label="Picture" error={errors.coverImage?.message}>
-            <input
-              {...register("coverImage", {
-                required: "The cover picture is required",
-              })}
-              type="file"
-              id="coverImage"
-              accept=".jpg,.jpeg"
-              className="form-input"
-            />
-          </FormField> */}
-
-          {/* <FormField label="Is this your Favorite Book?" error={errors.isFavorite?.message}>
-            <input
-              type="checkbox"
-              className="checkbox-input"
-              {...register("isFavorite", {
-                setValueAs: (value) => (value ? 1 : 0),
-              })}
-            />
-          </FormField> */}
-        </FieldSet>
-
-        <FormField>
-          <button className="form-button">Create new book</button>
-        </FormField>
-        <FormField>
-          <Link to="/books" className="cancel-button">
-            Cancel
+    <div className="modal-overlay">
+      <div className="modal-container">
+        <div className="modal-header-form-title">
+          <span>Create book</span>
+          <Link to={"/books"} className="link-btn">
+            <button className="close-button">×</button>
           </Link>
-        </FormField>
-      </form>
+        </div>
+        <div className="modal-body">
+          <form onSubmit={handleSubmit(submitForm)}>
+            <div className="form-group">
+              <label className="form-label">Title:</label>
+              <input
+                type="text"
+                className="form-input"
+                {...register("title", {
+                  required: "This field is required and cannot be empty",
+                  setValueAs: (value) => value.trim(),
+                  pattern: {
+                    value: /^[A-Za-zÀ-ÿ0-9\s.@]+$/,
+                    message: "Title must contain only letters and spaces",
+                  },
+                  maxLength: {
+                    value: 200,
+                    message: "Title cannot be longer than 200 characters",
+                  },
+                })}
+              />
+              {errors.title && (
+                <span className="error-message">{errors.title.message}</span>
+              )}
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">Author:</label>
+              <input
+                type="text"
+                className="form-input"
+                {...register("author", {
+                  required: "This field is required and cannot be empty",
+                  setValueAs: (value) => value.trim(),
+                  pattern: {
+                    value: /^[A-Za-zÀ-ÿ0-9\s.@]+$/,
+                    message: "Title must contain only letters and spaces",
+                  },
+                  maxLength: {
+                    value: 100,
+                    message: "Title cannot be longer than 100 characters",
+                  },
+                })}
+              />
+              {errors.author && (
+                <span className="error-message">{errors.author.message}</span>
+              )}
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">Year:</label>
+              <input
+                type="date"
+                className="form-input"
+                {...register("year", {
+                  required: "This field is required and cannot be empty",
+                })}
+              />
+              {errors.year && (
+                <span className="error-message">{errors.year.message}</span>
+              )}
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">Genres:</label>
+              <div className="checkbox-grid">
+                {["Fantasy", "Adventure", "Science Fiction", "Mystery"].map(
+                  (genre, index) => {
+                    const checkboxId = `cbx-genre-${index}`;
+                    return (
+                      <div className="checkbox-wrapper-15" key={genre}>
+                        <input
+                          className="inp-cbx"
+                          id={checkboxId}
+                          type="checkbox"
+                          style={{ display: "none" }}
+                          value={genre}
+                          {...register("genre")}
+                        />
+                        <label className="cbx" htmlFor={checkboxId}>
+                          <span>
+                            <svg width="12px" height="9px" viewBox="0 0 12 9">
+                              <polyline points="1 5 4 8 11 1"></polyline>
+                            </svg>
+                          </span>
+                          <span>{genre}</span>
+                        </label>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+
+            <div className="actions-container">
+              <Link to="/books" className="cancel-button">
+                Cancel
+              </Link>
+              <button type="submit" className="submit-btn">
+                Create new book
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/edit/Edit.tsx
+++ src/pages/edit/Edit.tsx
@@ -1,14 +1,12 @@
 import { useForm } from "react-hook-form";
-import { FieldSet } from "../../components/FormFields/FieldSet";
-import { FormField } from "../../components/FormFields/FormField";
 import { BookPropType } from "../../types/books/book.type";
 import { useAuth } from "../../context/UserContext";
 import { handlError } from "../../components/ErrorAlert/ErrorAlert";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import { useLibrary } from "../../context/LibraryContext";
 import { useEffect } from "react";
-import "../../components/FormFields/FormStyles.css";
 import { handleApiError } from "../../utils/handleApiErrors";
+import "../../components/FormFields/FormStyles.css";
 
 export const Edit = () => {
   const { edit, book, getOneBook } = useLibrary();
@@ -66,111 +64,119 @@ export const Edit = () => {
   const genreValues = watch("genre");
 
   return (
-    <div className="form-container">
-      <h1 className="form-header">Edit form</h1>
-      <form onSubmit={handleSubmit(submitForm)} className="form">
-        <FieldSet>
-          <FormField label="Title" error={errors.title?.message}>
-            <input
-              type="text"
-              placeholder="Title"
-              className="form-input"
-              {...register("title", {
-                required: "This field is required and cannot be empty",
-                setValueAs: (value) => value.trim(),
-                pattern: {
-                  value: /^[A-Za-zÀ-ÿ0-9\s.@]+$/,
-                  message: "Title must contain only letters and spaces",
-                },
-                maxLength: {
-                  value: 200,
-                  message: "Title cannot be longer than 200 characters",
-                },
-              })}
-            />
-          </FormField>
-
-          <FormField label="Author" error={errors.author?.message}>
-            <input
-              type="text"
-              placeholder="Author"
-              className="form-input"
-              {...register("author", {
-                required: "This field is required and cannot be empty",
-                setValueAs: (value) => value.trim(),
-                pattern: {
-                  value: /^[A-Za-zÀ-ÿ0-9\s.@]+$/,
-                  message: "Author must contain only letters and spaces",
-                },
-                maxLength: {
-                  value: 100,
-                  message: "Author cannot be longer than 100 characters",
-                },
-              })}
-            />
-          </FormField>
-
-          <FormField label="Year" error={errors.year?.message}>
-            <input
-              type="date"
-              placeholder="Year"
-              className="form-input"
-              {...register("year", {
-                required: "This field is required and cannot be empty",
-              })}
-            />
-          </FormField>
-
-          <div className="categories">
-            <p className="categories-header">Categories</p>
-            {["Fantasy", "Adventure", "Science Fiction", "Mystery"].map(
-              (genre, index) => {
-                const checkboxId = `cbx-genre-${index}`;
-                return (
-                  <div className="checkbox-wrapper-15" key={genre}>
-                    <input
-                      className="inp-cbx"
-                      id={checkboxId}
-                      type="checkbox"
-                      style={{ display: "none" }}
-                      value={genre}
-                      {...register("genre")}
-                      checked={genreValues?.includes(genre)} 
-                    />
-                    <label className="cbx" htmlFor={checkboxId}>
-                      <span>
-                        <svg width="12px" height="9px" viewBox="0 0 12 9">
-                          <polyline points="1 5 4 8 11 1"></polyline>
-                        </svg>
-                      </span>
-                      <span>{genre}</span>
-                    </label>
-                  </div>
-                );
-              }
-            )}
-          </div>
-
-          {/* <FormField label="Favorite" error={errors.isFavorite?.message}>
-            <input
-              type="checkbox"
-              className="checkbox-input"
-              {...register("isFavorite")}
-            />
-          </FormField> */}
-        </FieldSet>
-
-        <FormField>
-          <button type="submit" className="form-button">
-            Edit this book
-          </button>
-        </FormField>
-        <FormField>
-          <Link to="/books" className="cancel-button">
-            Cancel
+    <div className="modal-overlay">
+      <div className="modal-container">
+        <div className="modal-header-form-title">
+          <span>Create book</span>
+          <Link to={"/books"} className="link-btn">
+            <button className="close-button">×</button>
           </Link>
-        </FormField>
-      </form>
+        </div>
+        <div className="modal-body">
+          <form onSubmit={handleSubmit(submitForm)}>
+            <div className="form-group">
+              <label className="form-label">Title:</label>
+              <input
+                type="text"
+                className="form-input"
+                {...register("title", {
+                  required: "This field is required and cannot be empty",
+                  setValueAs: (value) => value.trim(),
+                  pattern: {
+                    value: /^[A-Za-zÀ-ÿ0-9\s.@]+$/,
+                    message: "Title must contain only letters and spaces",
+                  },
+                  maxLength: {
+                    value: 200,
+                    message: "Title cannot be longer than 200 characters",
+                  },
+                })}
+              />
+              {errors.title && (
+                <span className="error-message">{errors.title.message}</span>
+              )}
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">Author:</label>
+              <input
+                type="text"
+                className="form-input"
+                {...register("author", {
+                  required: "This field is required and cannot be empty",
+                  setValueAs: (value) => value.trim(),
+                  pattern: {
+                    value: /^[A-Za-zÀ-ÿ0-9\s.@]+$/,
+                    message: "Title must contain only letters and spaces",
+                  },
+                  maxLength: {
+                    value: 100,
+                    message: "Title cannot be longer than 100 characters",
+                  },
+                })}
+              />
+              {errors.author && (
+                <span className="error-message">{errors.author.message}</span>
+              )}
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">Year:</label>
+              <input
+                type="date"
+                className="form-input"
+                {...register("year", {
+                  required: "This field is required and cannot be empty",
+                })}
+              />
+              {errors.year && (
+                <span className="error-message">{errors.year.message}</span>
+              )}
+            </div>
+
+            <div className="form-group">
+              <label className="form-label">Genres:</label>
+              <div className="checkbox-grid">
+                {["Fantasy", "Adventure", "Science Fiction", "Mystery"].map(
+                  (genre, index) => {
+                    const checkboxId = `cbx-genre-${index}`;
+                    return (
+                      <div className="checkbox-wrapper-15" key={genre}>
+                        <input
+                          className="inp-cbx"
+                          id={checkboxId}
+                          type="checkbox"
+                          style={{ display: "none" }}
+                          value={genre}
+                          {...register("genre")}
+                          checked={genreValues?.includes(genre)}
+                        />
+                        <label className="cbx" htmlFor={checkboxId}>
+                          <span>
+                            <svg width="12px" height="9px" viewBox="0 0 12 9">
+                              <polyline points="1 5 4 8 11 1"></polyline>
+                            </svg>
+                          </span>
+                          <span>{genre}</span>
+                        </label>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+
+            <div className="actions-container">
+              <Link to="/books" className="cancel-button">
+                Cancel
+              </Link>
+              <button type="submit" className="submit-btn">
+                Create new book
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
